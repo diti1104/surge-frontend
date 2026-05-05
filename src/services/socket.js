0c087b00d2d9594d36ff48eb29bec9ac02408a0e
Srishti Raj <rajsrishti2004@gmail.com>
2026-05-05 08:52:13 +0530

    fix websocket pure ws

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,11 +1,10 @@
 import { Client } from '@stomp/stompjs';
-import SockJS from 'sockjs-client';
 
 let stompClient = null;
 
 export const connectSocket = (onMessage) => {
   stompClient = new Client({
-    webSocketFactory: () => new SockJS('https://surge-backend-production-fa1c.up.railway.app/ws'),
+    brokerURL: 'wss://surge-backend-production-fa1c.up.railway.app/ws',
     reconnectDelay: 5000,
     onConnect: () => {
       console.log('✅ WebSocket Connected!');
