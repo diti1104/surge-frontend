392709941df4c862412e42485b88e953b62021e5
Srishti Raj <rajsrishti2004@gmail.com>
2026-05-05 08:55:48 +0530

    revert to sockjs

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,16 +1,16 @@
 import { Client } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
 
 let stompClient = null;
 
 export const connectSocket = (onMessage) => {
   stompClient = new Client({
-    brokerURL: 'wss://surge-backend-production-fa1c.up.railway.app/ws',
+    webSocketFactory: () => new SockJS('https://surge-backend-production-fa1c.up.railway.app/ws'),
     reconnectDelay: 5000,
     onConnect: () => {
       console.log('✅ WebSocket Connected!');
       ['HIGH', 'MEDIUM', 'LOW'].forEach((zone) => {
         stompClient.subscribe(`/topic/surge/${zone}`, (msg) => {
-          console.log(`📨 Zone: ${zone}, Value: ${msg.body}`);
           const value = parseFloat(msg.body);
           onMessage(zone, value);
         });
@@ -18,9 +18,6 @@ export const connectSocket = (onMessage) => {
     },
     onDisconnect: () => {
       console.log('❌ WebSocket Disconnected!');
-    },
-    onStompError: (frame) => {
-      console.error('❌ STOMP error', frame);
     },
   });
   stompClient.activate();
